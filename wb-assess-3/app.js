0d874fa4da711eb916075c7bb5b5377c4af4e2eb
Kade Vaughan <kadevaughan6@gmail.com>
2023-10-26 14:51:38 -0600

    initial commit

--- wb-assess-3/app.js
+++ wb-assess-3/app.js
@@ -62,6 +62,17 @@ const OTHER_FOSSILS = [
 
 // TODO: Replace this comment with your code
 
+app.get('/', (req, res) => {
+  res.render('homepage.html.njk');
+});
+
+app.get('/top-fossils', req, res => {
+  const fossils = document.querySelector('MOST_LIKED_FOSSILS');
+  res.render('top-fossils.html.njk',{
+
+  })
+});
+
 app.get('/random-fossil.json', (req, res) => {
   const randomFossil = lodash.sample(OTHER_FOSSILS);
   res.json(randomFossil);
